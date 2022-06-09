edc63cadef7b9bb3311221af64a88831a1aed756
Surya Pratap Yadav <spyadav14699@gmail.com>
2022-06-09 16:18:54 +0530

    updating

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const User = require('../model/user');
 const bycript = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+
+
 //   adding user sighnup using bycrypt to encrypt the password 
 router.post('/sighnup', (req, res, next) => {
     bycript.hash(req.body.password, 10, (err, hash) => {
@@ -38,51 +41,66 @@ router.post('/sighnup', (req, res, next) => {
 });
 
 
-// adding user sighnin using bycrypt to encrypt the password
-router.post('/sighnin', (req, res, next) => {
-    User.find({ username: req.body.username })
-        .exec()
-        .then(user => {
-            if (user.length < 1) {
+// adding user login using bycrypt to encrypt the password
+router.post('/login', (req, res, next) => {
+    User.find({username: req.body.username})
+    .exec()
+    .then(user => {
+        if (user.length < 1) {
+
+            return res.status(401).json({
+                message: 'Authentication failed'
+            });
+        }
+
+        bycript.compare(req.body.password, user[0].password, (err, result) => {
+            if(!result) {
                 return res.status(401).json({
-                    message: 'Authentication failed'
+                    message: 'Authentication matching failed'
+                });
+
+            }
+            if(result) {
+                const token = jwt.sign({
+                    username: user[0].username,
+                    usertype: user[0].usertype,
+                    email: user[0].email,
+                    password: user[0].password
+
+
+                },
+                'secret',
+                {
+                    expiresIn: '24h'
+
+                }
+
+                );
+                return res.status(200).json({
+                    username: user[0].username,
+                    usertype: user[0].usertype,
+                    email: user[0].email,
+                    password: user[0].password,
+                    token: token
                 });
             }
-            bycript.compare(req.body.password, user[0].password, (err, result) => {
-                if (err) {
-                    return res.status(401).json({
-                        message: 'Authentication failed'
-                    });
-                }
-                if (result) {
-                    const token = jwt.sign({
-                        username: user[0].username,
-                        usertype: user[0].usertype
-                    },
-                        process.env.JWT_KEY,
-                        {
-                            expiresIn: "1h"
-                        }
-                    );
-                    return res.status(200).json({
-                        message: 'Authentication successful',
-                        token: token
-                    });
-                }
-                return res.status(401).json({
-                    message: 'Authentication failed'
-                });
-            });
-        }
-        ).catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
         }
         );
+    }
+    ).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+    );
 }
 );
+
+
+
+            
+
 
 
 
