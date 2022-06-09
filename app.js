131d98df3faa81f63d3948a0aeae72c28387b77e
Surya Pratap Yadav <spyadav14699@gmail.com>
2022-06-09 14:22:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ const facultyRouter = require('./api/routes/faculty');
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://spyadavg:Surya@123@cluster0.gi9rs.mongodb.net/?retryWrites=true&w=majority')
-
-mongoose.connect.on('error', (err) => {
-    console.log('Error in connecting to database', err);
-});
+mongoose.connect('mongodb+srv://spyadavg:JB0LbAYuPFpGBvGr@cluster0.gi9rs.mongodb.net/?retryWrites=true&w=majority');
 
 
-mongoose.connection.on('connected', () => {
+
+
+
+mongoose.connection.on('connected', (connected) => {
     console.log('connected to mongodb');
 });
 
+mongoose.connection.on('error', (err) => {
+    console.log('Error in connecting to database');
+});
 
 app.use('/faculty', facultyRouter);
 
@@ -25,11 +27,11 @@ app.use('/student', studentRouter);
 
 
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-})
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'It works!'
+//     });
+// })
 
 app.use((req, res, next) => {
     res.status(404).json({
