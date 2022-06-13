4d033843de19c7c0998d5a62724b9ba78889bd13
Surya Pratap Yadav <spyadav14699@gmail.com>
2022-06-13 15:47:38 +0530

    updating

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Skeleton from 'react-loading-skeleton';
 
 const Products = () => {
 
@@ -31,25 +32,89 @@ useEffect(() => {
 , []);
 
 
+
+
 const Loading = () => {
     return (
-        <>
-        Loading....
-        </>
+      <>
+        <div className="col-md-3">
+            <Skeleton height={350}/>
+        </div>  <div className="col-md-3">
+            <Skeleton height={350}/>
+        </div>  <div className="col-md-3">
+            <Skeleton height={350}/>
+        </div>  <div className="col-md-3">
+            <Skeleton height={350}/>
+        </div>
+      </>
+        
     )
 }
 
+const filterProduct = (catogery) => {
+    setFilter(data.filter(product => product.category === catogery));
+
+}
+
+
+
 const ShowProducts = () => {
   return (
-    <div className="button">
-    <button className="btn btn-outline-dark">All</button>
-    <button className="btn btn-outline-dark me-2">Men's clothing</button>
-    <button className="btn btn-outline-dark me-2">Women's clothing</button>
-    <button className="btn btn-outline-dark me-2">Electronics</button>
-    <button className="btn btn-outline-dark me-2">Makeup</button>
+   <>
+    <div className="button justify-content-center d-flex mb-5 pb-5">
+    <button className="btn btn-outline-dark me-2" onClick={
+        () => {
+            setFilter(data);
+        }
+    } >All</button>
+    <button className="btn btn-outline-dark me-2" onClick={
+        () => {
+            filterProduct("men's clothing");
+        }
+    }>Men's clothing</button>
+    <button className="btn btn-outline-dark me-2" onClick={
+        () => {
+            filterProduct("women's clothing");
+        }
+    }>Women's clothing</button>
+    <button className="btn btn-outline-dark me-2" onClick={
+        () => {
+            filterProduct("jewelery");
+        }
+    }>Jwellery</button>
+    <button className="btn btn-outline-dark me-2" onClick={
+        () => {
+            filterProduct("electronics");
+        }
+    }>Electronics</button>
    </div>
-  )
-}
+    {filter.map((el) => {
+
+
+        return (
+            <>
+    <div className="col-md-3">
+    <div className="card h-100 text-center p-4" key={el.id}>
+
+<img src={el.image} className="card-img-top" alt={el.title} height="210px"/>
+<div className="card-body">
+<h5 className="card-title mb-0">{el.title.substring(0,12)}</h5>
+<p className="card-text">${el.price}</p>
+<a href="#" className="btn btn-primary">Buy now</a>
+</div>
+</div>
+    </div>
+
+ </>
+    )
+    })}
+
+    </>
+    )}
+
+
+
+
 
 
   return (
@@ -63,7 +128,9 @@ const ShowProducts = () => {
 </div>
 
    <div className="row justify-content-center">
-{Loading ? <Loading/> : <ShowProducts/>}
+    {loading ? <Loading/> : <ShowProducts/>}
+         
+
    </div>
             </div>
     </div>
