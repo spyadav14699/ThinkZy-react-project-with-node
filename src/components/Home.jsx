4846139bedc1d7c0e428c20dd7d2abf8a8475d36
Surya Pratap Yadav <spyadav14699@gmail.com>
2022-06-13 13:53:48 +0530

    vfd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='hero'>
+
+<div className="card bg-dark text-white border-0">
+   
+  <img src="\webimages\girlimg.jpg" className="card-img" alt="background" height="550px/>
+  <div className="card-img-overlay">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p className="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
+
+    </div>
   )
 }
 
